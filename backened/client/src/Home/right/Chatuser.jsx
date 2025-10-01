7689dac55022514af204681fe2737b86a678ccf1
akash sharma <156177176+Aakashsh2607@users.noreply.github.com>
2025-10-01 11:28:50 +0530

    Update Chatuser.jsx

--- backened/client/src/Home/right/Chatuser.jsx
+++ backened/client/src/Home/right/Chatuser.jsx
@@ -1,40 +1,53 @@
 import React from "react";
-import useConversation from "../../statemanage/useConverstaion.js";
+import useConversation from "../../statemanage/useConversation.js";
 import { useSocketContext } from "../../context/SocketContext.jsx";
 import { CiMenuFries } from "react-icons/ci";
 
 function Chatuser() {
   const { selectedConversation } = useConversation();
   const { onlineUsers } = useSocketContext();
+
   const getOnlineUsersStatus = (userId) => {
     return onlineUsers.includes(userId) ? "Online" : "Offline";
   };
 
-   }
-console.log("Selected Conversation:", selectedConversation);
   return (
-    <div className=" pl-5 pt-5 h-[12vh] flex space-x-4 bg-gray-700 hover:bg-gray-600 duration-300">
-      <div>
-        <div className="avatar online">
-          <div className="w-14 rounded-full">
-            <img src="https://th.bing.com/th/id/OIP.Y2Z1sJCl2BKT65eecD0cQwAAAA?w=310&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" />
-          </div>
+    <div className="pl-5 pt-5 h-[12vh] flex items-center space-x-4 bg-gray-700 hover:bg-gray-600 duration-300">
+      {/* Avatar */}
+      <div className="avatar">
+        <div className="w-14 rounded-full">
+          <img
+            src={
+              selectedConversation?.profilePic ||
+              "https://cdn-icons-png.flaticon.com/512/149/149071.png"
+            }
+            alt="User Avatar"
+          />
         </div>
       </div>
-      <div>
 
-        <h1 className="text-xl">
-        {selectedConversation?.name ||
-          selectedConversation?.fullname ||
-          selectedConversation?.email ||
-          "User"}
-      </h1>
-        <span className="text-sm">
-          {getOnlineUsersStatus(selectedConversation._id)}
+      {/* User Info */}
+      <div className="flex flex-col justify-center">
+        <h2 className="text-white font-semibold text-lg">
+          {selectedConversation?.username || "Select a user"}
+        </h2>
+        <span
+          className={`text-sm font-medium ${
+            getOnlineUsersStatus(selectedConversation?._id) === "Online"
+              ? "text-green-400"
+              : "text-red-400"
+          }`}
+        >
+          {getOnlineUsersStatus(selectedConversation?._id)}
         </span>
+      </div>
+
+      {/* Menu Icon (right side) */}
+      <div className="ml-auto pr-5 cursor-pointer text-white text-2xl">
+        <CiMenuFries />
       </div>
     </div>
   );
-
+}
 
 export default Chatuser;
